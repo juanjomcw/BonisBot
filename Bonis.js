d88fee1113ab5380a5154efbd51fe403490d6cf1
juanjomcw <37007743+juanjomcw@users.noreply.github.com>
2018-06-22 01:02:16 -0600

    Update Bonis.js

--- Bonis.js
+++ Bonis.js
@@ -50,7 +50,7 @@ client.on("message", (message) => {
    message.channel.send('https://www.instagram.com/antonella.aservi');
  }else
     if (message.content.startsWith(prefix + "horario")){
-      message.channel.send("", {file:"https://gyazo.com/47839e77822f0597e7f81139a1eac90f"});
+      message.channel.send("Este es el Horario del Stream", {file:"https://cdn.discordapp.com/attachments/459460014952939530/459613908727103488/47839e77822f0597e7f81139a1eac90f.png"});
        
 }else
  if (message.content.startsWith(prefix + "yt")){
